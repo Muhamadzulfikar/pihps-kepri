92dec76585c4c9375d5b559e77419aba0d7a7b47
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2025-06-19 14:23:36 +0700

    Update: Menu items

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,9 +1,13 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i
+            class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 @if (auth()->user()->role === 'super admin' || auth()->user()->role === 'admin')
-<x-backpack::menu-item title="Komoditas Pasar Tradisional" icon="la " :link="backpack_url('commodities').'/tradisional'" />
-<x-backpack::menu-item title="Komoditas Pasar Modern" icon="la " :link="backpack_url('commodities').'/modern'" />
-<x-backpack::menu-item title="Komoditas Pasar Perdangan Besar" icon="la " :link="backpack_url('commodities').'/perdagangan_besar'" />
-<x-backpack::menu-item title="Komoditas Pasar Produsen" icon="la " :link="backpack_url('commodities').'/produsen'" />
+    <x-backpack::menu-item title="Kategori Kommoditas" icon="" :link="backpack_url('commodity-categories')"/>
+    <x-backpack::menu-item title="Kota / Kabupaten" icon="" :link="backpack_url('cities')"/>
+    <x-backpack::menu-item title="Pasar Kota" icon="" :link="backpack_url('city-markets')"/>
+    <x-backpack::menu-item title="Komoditas Pasar Tradisional" icon="" :link="backpack_url('commodities').'/tradisional'"/>
+    <x-backpack::menu-item title="Komoditas Pasar Modern" icon="" :link="backpack_url('commodities').'/modern'"/>
+    <x-backpack::menu-item title="Komoditas Pasar Perdangan Besar" icon="" :link="backpack_url('commodities').'/perdagangan_besar'"/>
+    <x-backpack::menu-item title="Komoditas Pasar Produsen" icon="" :link="backpack_url('commodities').'/produsen'"/>
 @endif
